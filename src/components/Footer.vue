b89a23267974852e3b5faa278e59fcb839ff29b7
remage18 <alexandr.chaban.65@gmail.com>
2023-10-11 17:22:06 +0300

    fix: header/footer add instructions.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,29 @@
 <template>
-    <v-footer app color="#222222">
-        <v-row justify="center" class="mt-2">
-            <v-btn
-                v-for="link in links"
-                :key="link"
-                variant="text"
-                :to="{ path: link[1]}"
-                class="mx-2"
-            >
-                {{ link[0] }}
-            </v-btn>
-            <v-col class="text-center mt-2" cols="12">
-                {{ new Date().getFullYear() }} — <strong>LiteStack</strong>
-            </v-col>
-        </v-row>
-    </v-footer>
+<!--    <v-footer app color="#222222">-->
+<!--            <v-col class="text-center ma-2">-->
+<!--                {{ new Date().getFullYear() }} — <strong>LiteStack</strong>-->
+<!--            </v-col>-->
+<!--    </v-footer>-->
+   <div class="footer">
+      <span>{{ new Date().getFullYear() }} — <strong>LiteStack</strong></span>
+   </div>
 </template>
+
+<style scoped>
+
+   .footer{
+      background-color: #222;
+      width: 100%;
+      height: 80px;
+      color: #fff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 18px;
+      letter-spacing: 3px;
+   }
+
+</style>
 
 <script>
 export default {
--- src/views/instructions/Instructions.vue
+++ src/views/instructions/Instructions.vue
@@ -3,48 +3,134 @@
 </script>
 
 <template>
-    <v-container>
-        <v-card>
-            <v-tabs
-                v-model="tab"
-                bg-color="dark"
-            fixed-tabs >
-                <v-tab value="servers">Сервери</v-tab>
-                <v-tab value="graphs">Графіки</v-tab>
-                <v-tab value="dbs">Бази даних</v-tab>
-                <v-tab value="dev">Розробка</v-tab>
+<!--      <v-container>-->
+<!--          <v-card>-->
+<!--              <v-tabs-->
+<!--                  v-model="tab"-->
+<!--                  bg-color="dark"-->
+<!--              fixed-tabs >-->
+<!--                  <v-tab value="servers">Сервери</v-tab>-->
+<!--                  <v-tab value="graphs">Графіки</v-tab>-->
+<!--                  <v-tab value="dbs">Бази даних</v-tab>-->
+<!--                  <v-tab value="dev">Розробка</v-tab>-->
 
-            </v-tabs>
+<!--              </v-tabs>-->
 
-            <v-card-text>
-                <v-window v-model="tab">
-                    <v-window-item value="servers">
-                        <Servers/>
-                    </v-window-item>
-                    <v-window-item value="graphs">
-                        Graphs
-                    </v-window-item>
-                    <v-window-item value="dbs">
-                        DBs
-                    </v-window-item>
-                    <v-window-item value="dev">
-                        Dev
-                    </v-window-item>
-                </v-window>
-            </v-card-text>
-        </v-card>
-    </v-container>
+<!--              <v-card-text>-->
+<!--                  <v-window v-model="tab">-->
+<!--                      <v-window-item value="servers">-->
+<!--                          <Servers/>-->
+<!--                      </v-window-item>-->
+<!--                      <v-window-item value="graphs">-->
+<!--                          Graphs-->
+<!--                      </v-window-item>-->
+<!--                      <v-window-item value="dbs">-->
+<!--                          DBs-->
+<!--                      </v-window-item>-->
+<!--                      <v-window-item value="dev">-->
+<!--                          Dev-->
+<!--                      </v-window-item>-->
+<!--                  </v-window>-->
+<!--              </v-card-text>-->
+<!--          </v-card>-->
+<!--      </v-container>-->
+
+      <div class="container">
+           <div class="info-container" v-for="service in services">
+                <div class="info-header" @click="service.showInfo = !service.showInfo;  rotate($event, service)">
+                     <span>{{ service.name }}</span>
+                     <div class="arrow"></div>
+                </div>
+             <p v-if="service.showInfo">{{service.text }}</p>
+           </div>
+      </div>
 </template>
-<style scoped>
 
+<style scoped>
+  .container{
+    margin: 50px 250px;
+  }
+  .info-container{
+    display: flex;
+    flex-direction: column;
+    border-bottom: 1px solid black;
+
+    padding: 25px 15px 25px 15px;
+  }
+  .info-header{
+    width: 100%;
+    padding-bottom: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    font-size: 24px;
+    letter-spacing: 4px;
+    padding-right: 10px;
+  }
+
+  .arrow{
+    border-width: 0 2px 2px 0;
+    transform: translate(60%, 0);
+    border-color: black;
+    border-style: solid;
+    width: 15px;
+    height: 15px;
+    rotate: 45deg;
+  }
+  .info-header:hover{
+    cursor: pointer;
+  }
 </style>
 
 <script>
 import Servers from "@/views/instructions/Servers.vue"
-
 export default {
     data: () => ({
         tab: null,
+        services : [
+          {
+            name: "СТВОРЕННЯ СЕРВЕРІВ",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n" +
+                "      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \n" +
+                "      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in \n" +
+                "      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat \n" +
+                "      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            showInfo: false,
+          },
+          {
+            name: "ІНТЕГРАЦІЯ МЕТОДІВ ОБРАХУНКУ ДАНИХ",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n" +
+                "      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \n" +
+                "      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in \n" +
+                "      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat \n" +
+                "      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            showInfo: false,
+          },
+          {
+            name: "ІНТЕГРАЦІЯ МЕТОДІВ ВІЗУАЛІЗАЦІЇ ДАНИХ",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n" +
+                "      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \n" +
+                "      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in \n" +
+                "      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat \n" +
+                "      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            showInfo: false,
+          },
+          {
+            name: "ІНТЕГРАЦІЯ БАЗ ДАНИХ",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n" +
+                "      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \n" +
+                "      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in \n" +
+                "      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat \n" +
+                "      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            showInfo: false,
+          },
+        ],
+
+        rotate: function (e, s){
+          if(s.showInfo === true) {
+            e.currentTarget.children[1].style.cssText = `transform: translate(90%, 40%); border-width: 2px 0 0 2px; `;
+          } else e.currentTarget.children[1].style.cssText = `transform: translate(60%, 0); border-width: 0 2px 2px 0; `;
+        }
     }),
     components: {
         Servers,
